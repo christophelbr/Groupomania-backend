e926a9b7a14822df6a971832d2ecb618b7e5445a
christophelbr <c.lebren@icloud.com>
2021-05-08 10:44:45 +0200

    création serveur

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Projet 7 OpenClassrooms : Créer un réseau social d'entreprise Groupomania
 Backend
+
+Serveur NodeJs dépendances :
+    - nodemon
+    - express
+    - body-parser
---
+++ app.js
@@ -0,0 +1,25 @@
+// Importation des modules
+const express = require('express'); // ajout du routeur express
+const helmet = require('helmet');
+const bodyParser = require('body-parser'); //ajout de body-parser au projet : permet extraction d'objet JSON
+
+// Importation des routes
+
+// Application express
+const app = express();
+
+
+// Correction des erreurs de CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+// Protection de l'application avec helmet
+app.use(helmet());
+
+// Routes
+
+module.exports = app;
